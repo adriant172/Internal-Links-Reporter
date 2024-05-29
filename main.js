473a4e2893475a9c75f4ced66c51caf551f6c76d
Matthew Thomas <adriant172@gmail.com>
2024-05-29 23:34:51 +0900

    add intial crawl module and its corresponding tests, as well as bootstrap the main function

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-console.log('Hello World!!')
+import { argv } from 'node:process';
+
+function main() {
+    const args = argv.slice(2)
+    if (args.length < 1) {
+        throw new Error("No arguments were provided.")
+    } else if (args.length > 1) {
+        throw new Error(" Too many arguments were provided.")
+    } else {
+        console.log(`This is the base URL: ${args[0]}`)
+    }
+  }
+  
+  main()
+  
